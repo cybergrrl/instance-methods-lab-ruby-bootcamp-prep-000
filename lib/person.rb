58bb244fbb6a5ac060f71bdb1d438cc6843a8981
scout b <scout.burghardt@gmail.com>
2018-10-10 14:41:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,10 @@ class Person
   end
   
   def walk
+<<<<<<< HEAD
     puts "The Person is walking"
+=======
+    puts "The Person is Walking"
+>>>>>>> 5908798481e4fcd5fe2e5c57cc942a37229fc8c6
   end
 end
